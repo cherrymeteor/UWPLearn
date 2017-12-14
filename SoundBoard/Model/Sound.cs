fbd413bb8f3d51b2d2e92e8843678577c2be3eb5
cherrymeteor <cherrymeteor@outlook.com>
2017-12-14 12:06:13 +0800

    初始版本

---
+++ SoundBoard/Model/Sound.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundBoard.Model
+{
+    class Sound
+    {
+        private string name;
+        private SoundCategory category;
+        private string audioFile;
+        private string imageFile;
+
+        public string Name { get => name; set => name = value; }
+        public SoundCategory Category { get => category; set => category = value; }
+        public string AudioFile { get => audioFile; set => audioFile = value; }
+        public string ImageFile { get => imageFile; set => imageFile = value; }
+
+        public Sound(string name)
+        {
+        }
+    }
+
+    public class SoundCategory
+    {
+    }
+}
