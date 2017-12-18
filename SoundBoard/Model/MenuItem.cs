9933226aca17b52f51afd44d2f9334cbe25258b4
cherrymeteor <cherrymeteor@outlook.com>
2017-12-18 10:19:06 +0800

    亚克力特效简单使用

---
+++ SoundBoard/Model/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SoundBoard.Model
+{
+    public class MenuItem
+    {
+        public string IconFile { get; set;}
+        public SoundCategory Category { get; set; }
+    }
+}
---
+++ SoundBoard/Model/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundBoard.Model
+{
+    public class SoundManager
+    {
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            sounds.Clear();
+            getSounds().ForEach(p => sounds.Add(p));
+        }
+        
+        public static void GetSoundsByCategory(ObservableCollection<Sound> sounds, SoundCategory soundCategory)
+        {
+            var filteredSounds = getSounds().Where(p => p.Category == soundCategory).ToList();
+            sounds.Clear();
+            filteredSounds.ForEach(p => sounds.Add(p));
+        }
+
+        private static List<Sound> getSounds()
+        {
+            var sounds = new List<Sound>
+            {
+                new Sound("Cow", SoundCategory.Animals),
+                new Sound("Cat", SoundCategory.Animals),
+                new Sound("Gun", SoundCategory.Cartoons),
+                new Sound("Spring", SoundCategory.Cartoons),
+                new Sound("Clock", SoundCategory.Taunts),
+                new Sound("LOL", SoundCategory.Taunts),
+                new Sound("Ship", SoundCategory.Warnings),
+                new Sound("Siren", SoundCategory.Warnings)
+            };
+
+            return sounds;
+        }
+    }
+}
